b4a710882b4b65623eaecd0b9e9c5a67442de8c6
Bastian Krones <bkrones.uni@gmail.com>
2017-01-24 19:19:02 +0100

    Reverting passwords

--- databases/dbCredentials.php
+++ databases/dbCredentials.php
@@ -3,7 +3,7 @@
 [fpraktikum]
 link=localhost
 username=fpraktikum
-passwd="***REMOVED***1"
+passwd="7SA&JbluEB%h!1TneR0q"
 dbname=fpraktikum-test
 tbl-anmeldung=anmeldungen
 tbl-angebote=angebote
@@ -14,7 +14,7 @@ tbl-partners="tbl_partners"
 [ilias]
 link=localhost
 username=ilias
-passwd="***REMOVED***1"
+passwd="qcSTvtvJsfxhP4HQ"
 dbname="ilias-dev-4.3"
 tbl-name="usr_data"
 col-name="usr_id"
