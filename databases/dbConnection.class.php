6f15be660210aba4720b62f0c1b5ab7a4d51af8f
Bastian Krones <bkrones.uni@gmail.com>
2017-01-24 18:21:14 +0100

    Updated dbConnection

--- databases/dbConnection.class.php
+++ databases/dbConnection.class.php
@@ -7,38 +7,15 @@
  */
 class DbConnection
 {
-  protected $dbLink;
-  protected $dbUser;
-  protected $dbPassword;
-  protected $dbTable;
   protected $db;
 
   public function __construct($dbLink, $dbName, $dbUser, $dbPassword)
   {
-    $this->dbLink = $dbLink;
-    $this->dbTable = $dbName;
-    $this->dbUser = $dbUser;
-    $this->dbPassword = $dbPassword;
-	
-	  $this->initDb();
-  }
-
-  public function __destruct()
-  {
-      // throws error $this->db would be undefined
-      //$this->db->close();
-  }
-
-  public function initDb()
-  {
-    $pdo = new PDO('pgsql:host=' . $this->dbLink . ';port=5432;dbname=' . $this->dbName, 'anyuser', 'pw');
-    $stmt = $pdo->prepare('SELECT * FROM sometable');
-    $stmt->execute();
-    $pdo = null;
-    $this->db = new mysqli($this->dbLink, $this->dbUser, $this->dbPassword, $this->dbTable)
-    or die("Unable to connect to Database with link ".$this->dbLink."!");
-
-    $this->db->set_charset('UTF8');
+    try{
+	    $this->db = new PDO('mysql:host=' . $dbLink . ';dbname=' . $dbName, $dbUser, $dbPassword);
+    } catch(PDOException $e) {
+      echo 'Konnte keine Verbinung herstellen: ' . $e->getMessage();
+    }
   }
 
   public function prepare($query)
@@ -48,5 +25,11 @@ class DbConnection
       die("Error ".$this->db->error);
     }
     return $stmt; 
+  }
+
+  public function __destruct()
+  {
+      // throws error $this->db would be undefined
+      //$this->db->close();
   }
 }
--- databases/dbCredentials.php
+++ databases/dbCredentials.php
@@ -1,20 +1,20 @@
-***REMOVED***
+; <?php exit(); ?>
 
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
+[fpraktikum]
+link=localhost
+username=fpraktikum
+passwd="***REMOVED***"
+dbname=fpraktikum-test
+tbl-anmeldung=anmeldungen
+tbl-angebote=angebote
+tbl-registration="tbl_registrations"
+tbl-courses="tbl_courses"
+tbl-partners="tbl_partners"
 
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
+[ilias]
+link=localhost
+username=ilias
+passwd="***REMOVED***"
+dbname="ilias-dev-4.3"
+tbl-name="usr_data"
+col-name="usr_id"
